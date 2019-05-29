73eee33694ebaa9b96b49e482e0fe2bc36396b78
GaoLei <gaolei@gaoleideMacBook-Pro-2.local>
2019-05-29 15:57:08 +0800

    实现greendao

--- greendao/src/main/java/com/gaolei/greendao/MainActivity.java
+++ greendao/src/main/java/com/gaolei/greendao/MainActivity.java
@@ -2,6 +2,12 @@ package com.gaolei.greendao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gaolei.greendao.db.DBManager;
+import com.gaolei.greendao.db.SearchRecord;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DBManager dbManager = DBManager.getInstance(MainActivity.this);
+        for (int i = 0; i < 10; i++) {
+            SearchRecord searchRecord = new SearchRecord();
+            searchRecord.setName("测试数据" + i);
+            dbManager.insertUser(searchRecord);
+        }
+
+        List<SearchRecord> recordList = DBManager.getInstance(this).queryUserList();
+
+        Toast.makeText(this, "数据条数：" + recordList.size(), Toast.LENGTH_SHORT).show();
+        ;
     }
 }
